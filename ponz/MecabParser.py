35a6c38750178a160001bbc7c3b12e8a863dc3d9
dev <dev@tomoyasu-MacBook-Pro.local>
2016-10-15 10:05:23 +0900

    fix

--- ponz/MecabParser.py
+++ ponz/MecabParser.py
@@ -88,8 +88,8 @@ class MecabParser:
 
 
     def check_unnecessary(self, noun):
-        string = noun.decode('utf-8')
-        #string = unicode(noun, 'utf-8')
+        #string = noun.decode('utf-8')
+        string = unicode(noun, 'utf-8')
         kanji = re.search(u'[一-龠]', string)
         hiragana = re.search(u'[ぁ-ん]', string)
         katakana = re.search(u'[ァ-ヴ]', string)
